1a062e39c184aef72fefc3b99c344cb79b06e50f
Aniket Bhogawar <39596152+1aniket@users.noreply.github.com>
2026-05-08 01:06:37 +0530

    Feat : Add Health Feature

--- server/index.js
+++ server/index.js
@@ -16,19 +16,23 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("DB connected"))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/questions", questionRoutes);
-
 
 //Routes which require authentication.
 app.get("/api/protected", protect, (req, res) => {
   res.json({ message: "You are authorized", user: req.user });
 });
 
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "running ok !" });
+});
 app.post("/api/run", runCode);
 
 app.listen(5000, () => console.log("Server running on port 5000"));
